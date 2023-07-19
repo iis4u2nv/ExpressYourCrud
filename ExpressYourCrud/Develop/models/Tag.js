e8ac0fca32497b91d1a6000ff0aa7a76708fe30f
amazing grace <agracenotary@gmail.com>
2023-07-18 21:02:22 -0400

    created files

---
+++ ExpressYourCrud/Develop/models/Tag.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('./config/connection.js');
+
+class Tag extends Model {}
+// define columns
+Tag.init(
+  {    
+   id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  tag_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  tag_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'tag',
+      key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'tag'
+  },
+);
+
+module.exports = Tag;
